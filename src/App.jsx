41ac1c630c0f54a5d8e932ee4802982a498f44bc
Nguyễn Quang Hoàng <datinhcombat@gmail.com>
2017-05-06 10:25:01 +0700

    add structure

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// eslint-disable-next-line
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      a: 2,
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <div classNamsdfsde="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Reacsdfsdfsdt</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
